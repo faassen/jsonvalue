221ccd2dfec84801ba370363350107ea2788211a
faassen <faassen@startifact.com>
2014-09-25 11:28:50 +0200

    New empty project with working buildout.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    open('CHANGES.txt').read())
+
+tests_require = [
+    'pytest >= 2.0',
+    'pytest-cov',
+    ]
+
+setup(
+    name='jsonvalue',
+    version='0.1.dev0',
+    description="Rich values for JSON",
+    long_description=long_description,
+    author="Martijn Faassen",
+    author_email="faassen@startifact.com",
+    license="BSD",
+    keywords='jsonvalue',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'pyld',
+    ],
+    tests_require=tests_require,
+    extras_require=dict(
+        test=tests_require,
+    )
+)
